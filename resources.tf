d4f5f802417fc6b1b8563c7d1adb620534ee76b2
SkyMech66 <106203458+SkyMech66@users.noreply.github.com>
2022-12-12 14:15:18 -0800

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,15 +1,15 @@
-resource "aws_vpc" "Wk23" {
+resource "aws_vpc" "Wk22" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "Wk23"
+    Name = "Wk22"
   }
 }
 
 resource "aws_subnet" "public_subnets" {
  count      = length(var.public_subnet_cidrs)
- vpc_id     = aws_vpc.Wk23.id
+ vpc_id     = aws_vpc.Wk22.id
  cidr_block = element(var.public_subnet_cidrs, count.index)
  availability_zone = element(var.azs, count.index)
  
@@ -17,17 +17,17 @@ resource "aws_subnet" "public_subnets" {
 
 resource "aws_subnet" "private_subnets" {
  count      = length(var.private_subnet_cidrs)
- vpc_id     = aws_vpc.Wk23.id
+ vpc_id     = aws_vpc.Wk22.id
  cidr_block = element(var.private_subnet_cidrs, count.index) 
  availability_zone = element(var.azs, count.index)
  }
  
 resource "aws_internet_gateway" "gw" {
- vpc_id = aws_vpc.Wk23.id
+ vpc_id = aws_vpc.Wk22.id
 }
 
 resource "aws_route_table" "second_rt" {
- vpc_id = aws_vpc.Wk23.id
+ vpc_id = aws_vpc.Wk22.id
  
  route {
    cidr_block = "0.0.0.0/0"
